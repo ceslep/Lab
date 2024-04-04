7d732ad3f9409cb8d98a4471227a7e23a2262cec
ceslep <ceslep@gmail.com>
2024-04-04 17:54:43 -0500

    lab2

--- lib/pages/home_laboratorio.dart
+++ lib/pages/home_laboratorio.dart
@@ -20,14 +20,7 @@ class HomeLaboratorio extends StatefulWidget {
 
 class _HomeLaboratorioState extends State<HomeLaboratorio> {
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    setState(() {});
   }
 
   @override
